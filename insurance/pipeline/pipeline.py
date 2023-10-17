937b7a7a72ef83341c65e64cba09a0ca6ad0b155
Manan <manan511shah@gmail.com>
2023-10-17 15:37:58 +0530

    Data Validation Stage added and tested

--- insurance/pipeline/pipeline.py
+++ insurance/pipeline/pipeline.py
@@ -2,8 +2,9 @@ import os,sys
 from insurance.logger import logging
 from insurance.exception import InsuranceException
 from insurance.config.configuration import Configuration
-from insurance.entity.artifact_entity import DataIngestionArtifact
+from insurance.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
 from insurance.components.data_ingestion import DataIngestion
+from insurance.components.data_validation import DataValidation
 
 class Pipeline:
 
@@ -20,8 +21,17 @@ class Pipeline:
         except Exception as e:
             raise InsuranceException(sys,e) from e
         
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
+        try:
+            data_validation = DataValidation(data_validation_config=self.config.get_data_validation_config(),
+                                             data_ingestion_artifact=data_ingestion_artifact)
+            return data_validation.intiate_data_validation()
+        except Exception as e:
+            raise InsuranceException(sys,e) from e
+        
     def run_pipeline(self):
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception as e:
             raise InsuranceException(sys,e) from e
